5be7c2930810464cdff1907669adfee3722a9c03
WagnoLeaoSergio <WagnoLeaoSergio@users.noreply.github.com>
2022-01-19 11:21:49 +0000

    ✅ Ready to clone and code.

--- portfolio_auto_analysis/base.py
+++ portfolio_auto_analysis/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+portfolio_auto_analysis base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the portfolio_auto_analysis project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
