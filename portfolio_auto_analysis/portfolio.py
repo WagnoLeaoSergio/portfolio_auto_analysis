541bc782d7c2580ecd96e85fd051f42627d78466
WagnoLeaoSergio <wagnoleao@gmail.com>
2022-01-24 17:39:15 -0300

    Class Portfolio_Analyzer created:
    - Functions now are methods of this class

--- portfolio_auto_analysis/portfolio.py
+++ portfolio_auto_analysis/portfolio.py
@@ -5,130 +5,135 @@ import seaborn as sb
 from datetime import date
 from nsepy import get_history
 
-def get_price_data(symbols:list) -> pd.DataFrame:
-    start_date = date(2021,1,19)
-    end_date = date.today()
-    print(end_date)
-    print(f"You have {len(symbols)} assets in your portfolio.")
+class Portfolio_Analyzer():
+    def __init__(self):
+        self.graphs_folder = 'graphs/'
+        pass
 
-    # Fetching data
-    data_frame = pd.DataFrame()
-    for i, symbol in enumerate(symbols):
-        data = get_history(
-            symbol=symbol,
-            start=start_date,
-            end=end_date
-        )[['Symbol', 'Close']]
-        
-        data.rename(
-            columns={ 'Close': data['Symbol'][0] },
-            inplace=True
+    def get_price_data(self, symbols:list) -> pd.DataFrame:
+        start_date = date(2021,1,19)
+        end_date = date.today()
+        print(end_date)
+        print(f"You have {len(symbols)} assets in your portfolio.")
+
+        # Fetching data
+        data_frame = pd.DataFrame()
+        for i, symbol in enumerate(symbols):
+            data = get_history(
+                symbol=symbol,
+                start=start_date,
+                end=end_date
+            )[['Symbol', 'Close']]
+            
+            data.rename(
+                columns={ 'Close': data['Symbol'][0] },
+                inplace=True
+            )
+
+            data.drop(['Symbol'], axis=1, inplace=True)
+
+            if i == 0:
+                data_frame = data
+            else:
+                data_frame = data_frame.join(data)
+        return data_frame
+
+    def plot_history_graph(self, data_frame: pd.DataFrame) -> None:
+        fig, ax = plt.subplots(figsize=(15,8))
+        for i in data_frame.columns.values:
+            ax.plot(data_frame[i], label=i)
+
+        ax.set_title("Portfolio Close Price History")
+        ax.set_xlabel('Date', fontsize=18)
+        ax.set_ylabel('Close Price INR (Rs)', fontsize=18)
+        ax.legend(data_frame.columns.values, loc='upper left')
+        plt.savefig(self.graphs_folder + 'symbols_prices_history.png')
+
+
+    def plot_correlation_matrix(self, data_frame: pd.DataFrame) -> None:
+        correlation_matrix = data_frame.corr(method='pearson')
+        print('Correlation between Symbols in your portfolio')
+        print(correlation_matrix)
+
+        fig = plt.figure(figsize=(12,7))
+        sb.heatmap(
+            correlation_matrix,
+            xticklabels=correlation_matrix.columns,
+            yticklabels=correlation_matrix.columns,
+            cmap='YlGnBu',
+            annot=True,
+            linewidth=0.5
         )
+        plt.savefig(self.graphs_folder + 'correlation_matrix.png')
 
-        data.drop(['Symbol'], axis=1, inplace=True)
-
-        if i == 0:
-            data_frame = data
-        else:
-            data_frame = data_frame.join(data)
-    return data_frame
-
-def plot_history_graph(data_frame: pd.DataFrame) -> None:
-    graphs_folder = 'graphs/'
-
-    fig, ax = plt.subplots(figsize=(15,8))
-    for i in data_frame.columns.values:
-        ax.plot(data_frame[i], label=i)
-
-    ax.set_title("Portfolio Close Price History")
-    ax.set_xlabel('Date', fontsize=18)
-    ax.set_ylabel('Close Price INR (Rs)', fontsize=18)
-    ax.legend(data_frame.columns.values, loc='upper left')
-    plt.savefig(graphs_folder + 'symbols_prices_history.png')
-
-
-def plot_correlation_matrix(data_frame: pd.DataFrame) -> None:
-    graphs_folder = 'graphs/'
-
-    correlation_matrix = data_frame.corr(method='pearson')
-    print('Correlation between Symbols in your portfolio')
-    print(correlation_matrix)
-
-    fig = plt.figure(figsize=(12,7))
-    sb.heatmap(
-        correlation_matrix,
-        xticklabels=correlation_matrix.columns,
-        yticklabels=correlation_matrix.columns,
-        cmap='YlGnBu',
-        annot=True,
-        linewidth=0.5
-    )
-    plt.savefig(graphs_folder + 'correlation_matrix.png')
-
-def generate_periodic_simple_returns(
-        data_frame: pd.DataFrame,
-        period: int
+    def generate_periodic_simple_returns(
+            self,
+            data_frame: pd.DataFrame,
+            period: int
     ) -> pd.DataFrame:
-    return data_frame.pct_change(period).dropna()
+        return data_frame.pct_change(period).dropna()
 
-def plot_periodic_simple_returns(
-        psr: pd.DataFrame
+    def plot_periodic_simple_returns(
+            self,
+            psr: pd.DataFrame
     ) -> None:
-    graphs_folder = 'graphs/'
+        fig, ax = plt.subplots(figsize=(15, 8))
 
-    fig, ax = plt.subplots(figsize=(15, 8))
+        for i in psr.columns.values:
+            ax.plot(psr[i], lw=2, label=i)
 
-    for i in psr.columns.values:
-        ax.plot(psr[i], lw=2, label=i)
+        ax.legend(loc='upper right', fontsize=10)
+        ax.set_title('Volatility in periodic simple returns')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Periodic simple returns')
 
-    ax.legend(loc='upper right', fontsize=10)
-    ax.set_title('Volatility in periodic simple returns')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Periodic simple returns')
+        plt.savefig(self.graphs_folder + 'periodic_simple_returns.png')
 
-    plt.savefig(graphs_folder + 'periodic_simple_returns.png')
+    def generate_average_PSR(self, psr: pd.DataFrame) -> pd.DataFrame:
+        return psr.mean()
 
-def generate_average_PSR(psr: pd.DataFrame) -> pd.DataFrame:
-    return psr.mean()
+    def plot_PSR_risk(self, psr: pd.DataFrame) -> None:
 
-def plot_PSR_risk(psr: pd.DataFrame) -> None:
-    graphs_folder = 'graphs/'
-
-    print('Periodic Returns Risk')
-    psr.plot(
-        kind='box',
-        figsize=(20,10),
-        title="Risk Box Plot"
-    )
-    plt.savefig(graphs_folder + 'risk_box_plot.png')
-
-def generate_annualized_standard_deviantion(psr: pd.DataFrame, days: int):
-    return (psr.std() * np.sqrt(days))
-
-def sharpe_ratio(avg_psr: pd.DataFrame, std: pd.DataFrame) -> pd.DataFrame:
-    return (avg_psr / std) * 100
-
-def cummulative_PSR(psr: pd.DataFrame) -> pd.DataFrame:
-    return (psr + 1).cumprod()
-
-def plot_cummulative_PSR(c_psr: pd.DataFrame) -> None:
-    graphs_folder = 'graphs/'
-
-    fig, ax = plt.subplots(figsize=(18,8))
-    
-    for i in c_psr.columns.values:
-        ax.plot(
-                c_psr[i],
-                lw=2,
-                label=i
+        print('Periodic Returns Risk')
+        psr.plot(
+            kind='box',
+            figsize=(20,10),
+            title="Risk Box Plot"
         )
+        plt.savefig(self.graphs_folder + 'risk_box_plot.png')
 
-    ax.legend(loc='upper left', fontsize=10)
-    ax.set_title('Periodic Cummulative Simple returns/growth of investment')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Growth of Rs 1 investment')
+    def generate_annualized_standard_deviantion(
+            self,
+            psr: pd.DataFrame, days: int
+    ):
+        return (psr.std() * np.sqrt(days))
 
-    plt.savefig(graphs_folder + 'cummulative_returns.png')
+    def sharpe_ratio(
+            self,
+            avg_psr: pd.DataFrame,
+            std: pd.DataFrame
+    ) -> pd.DataFrame:
+        return (avg_psr / std) * 100
+
+    def cummulative_PSR(self, psr: pd.DataFrame) -> pd.DataFrame:
+        return (psr + 1).cumprod()
+
+    def plot_cummulative_PSR(self, c_psr: pd.DataFrame) -> None:
+        fig, ax = plt.subplots(figsize=(18,8))
+        
+        for i in c_psr.columns.values:
+            ax.plot(
+                    c_psr[i],
+                    lw=2,
+                    label=i
+            )
+
+        ax.legend(loc='upper left', fontsize=10)
+        ax.set_title('Periodic Cummulative Simple returns/growth of investment')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Growth of Rs 1 investment')
+
+        plt.savefig(self.graphs_folder + 'cummulative_returns.png')
 
 def proto() -> None:
     plt.style.use('fivethirtyeight')
@@ -146,47 +151,57 @@ def proto() -> None:
         'RELIANCE'
     ]
 
-    data_frame = get_price_data(stocksymbols)
+    analyzer = Portfolio_Analyzer();
+
+    data_frame = analyzer.get_price_data(stocksymbols)
     print(data_frame.head())
 
     # Analysis
 
     # Price History
-    plot_history_graph(data_frame)
+    analyzer.plot_history_graph(data_frame)
 
     # Correlation Matrix
-    plot_correlation_matrix(data_frame)
+    analyzer.plot_correlation_matrix(data_frame)
 
     # Daily Simple Returns
     print('Daily Simple Returns:')
-    daily_simple_return = generate_periodic_simple_returns(data_frame, 1)
+    daily_simple_return = analyzer.generate_periodic_simple_returns(
+            data_frame,
+            1
+    )
     print(daily_simple_return.head())
 
-    plot_periodic_simple_returns(daily_simple_return)
+    analyzer.plot_periodic_simple_returns(daily_simple_return)
 
 
     # Avrage Daily returns
     print('Average Daily returns(%) of stocks in your portfolio')
-    avg_daily = generate_average_PSR(daily_simple_return)
+    avg_daily = analyzer.generate_average_PSR(daily_simple_return)
     print(avg_daily*100)
 
     # Risk
-    plot_PSR_risk(daily_simple_return)
+    analyzer.plot_PSR_risk(daily_simple_return)
 
     print('Annualized Standard Deviation (Volatility(%), 252 trading days) ' +
         'of individual stocks in your portfolio on the basis of daily simple ' +
         'returns.')
-    standard_deviation = generate_annualized_standard_deviantion(
+    standard_deviation = analyzer.generate_annualized_standard_deviantion(
         daily_simple_return,
         252
     )
 
     print('Sharpe Ratio')
-    return_per_unit_risk = sharpe_ratio(avg_daily, standard_deviation)
+    return_per_unit_risk = analyzer.sharpe_ratio(
+            avg_daily,
+            standard_deviation
+    )
     print(return_per_unit_risk)
     
     # Cumulative returns
     print('Cummulative Returns:')
-    daily_cummulative_simple_return = cummulative_PSR(daily_simple_return)
+    daily_cummulative_simple_return = analyzer.cummulative_PSR(
+            daily_simple_return
+    )
     print(daily_cummulative_simple_return)
-    plot_cummulative_PSR(daily_cummulative_simple_return)
+    analyzer.plot_cummulative_PSR(daily_cummulative_simple_return)
