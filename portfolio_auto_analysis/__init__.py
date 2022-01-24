c9584194356f529454f167cc2f6fd3be7cea19be
WagnoLeaoSergio <wagnoleao@gmail.com>
2022-01-24 18:00:50 -0300

    portfolio module added

--- portfolio_auto_analysis/__init__.py
+++ portfolio_auto_analysis/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseClass, base_function
+from .portfolio import proto, Portfolio_Analyzer
 
-__all__ = ["BaseClass", "base_function"]
+__all__ = ["BaseClass", "base_function", "proto", "Portfolio_Analyzer"]
