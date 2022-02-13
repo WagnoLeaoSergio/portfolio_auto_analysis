b40182f130780159e8852e22b635791232965fed
WagnoLeaoSergio <wagnoleao@gmail.com>
2022-02-12 21:36:01 -0300

    Optmizer class updated to receive parameters from outside

--- portfolio_auto_analysis/optimization.py
+++ portfolio_auto_analysis/optimization.py
@@ -1,33 +1,32 @@
 import pygad
 import numpy as np
+import pandas as pd
 from datetime import date
 import matplotlib.pyplot as plt
-from . import Portfolio_Analyzer
+from portfolio import Portfolio_Analyzer
+from timeit import default_timer as timer
 
 class Optmizer():
-    def __init__(self):
+    def __init__(
+        self,
+        symbols,
+        parameters=None,
+    ):
+        self.symbols = symbols
         self.__has_runned = False
-        self.solution = []
         self.filename = 'opt_results/genetic' # The filename to which the instance is saved. The name is without extension.
         last_fitness = 0
-        pass
+        self.parameters = parameters if parameters else {}
+        self.results = {}
 
-    def run(self):
+    def run(self, assets_data=pd.DataFrame([])):
         global last_fitness
         global fitness_func
         global on_generation
 
         # Defining parameters
         wallet = 1000
-        stocksymbols = [
-            'AAPL',
-            'AMZN',
-            'MSFT',
-            'EOG',
-            'OXY',
-            'HAL',
-            'SLB'
-        ]
+
         pygad.random.seed(1)
         pygad.numpy.random.seed(1)
         analyzer = Portfolio_Analyzer()
@@ -35,23 +34,30 @@ class Optmizer():
         end_date = date.today()
         years_diff = (end_date - start_date).days / 365
 
-        assets_data = analyzer.get_price_data(
-                stocksymbols,
-                start_date,
-                end_date
-        )
+        if assets_data.empty:
+            assets_data = analyzer.get_price_data(
+                    self.symbols,
+                    self.parameters['start_date'],
+                    self.parameters['end_date']
+            )
+
+        rf = self.parameters['risk_free_rate'] if self.parameters['risk_free_rate'] else 0.0697
 
         def fitness_func(solution, solution_idx):
             if np.sum(solution) < 0 or np.sum(solution) > 1:
                 return 0
+            if any([ perc < 0.1 for perc in solution ]):
+                return 0
 
             def allocation(row):
                 return sum([ value*solution[i] for i, value in enumerate(row) ])
+
             daily_return = assets_data.pct_change(1).dropna()
             daily_return['Portfolio'] = daily_return.apply(allocation, axis=1) 
+
             num_days = 255 * years_diff if years_diff else 255
 
-            return analyzer.f_sortino_ratio(daily_return['Portfolio'], num_days, 0.0697)
+            return analyzer.f_sharpe_ratio(daily_return['Portfolio'], num_days, rf)
 
         def on_generation(ga_instance):
             global last_fitness
@@ -60,10 +66,10 @@ class Optmizer():
             print("Change     = {change}".format(change=ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1] - last_fitness))
             last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
 
-        num_generations = 2000 # Number of generations.
+        num_generations = 1000 # Number of generations.
         num_parents_mating = 15 # Number of solutions to be selected as parents in the mating pool.
         sol_per_pop = 40 # Number of solutions in the population.
-        num_genes = len(stocksymbols)
+        num_genes = len(self.symbols)
         init_low_range = 0.0
         init_high_range = 1.0
         random_mutation_min_val = 0.0
@@ -73,10 +79,10 @@ class Optmizer():
 
         ga_instance = pygad.GA(
             gene_space=gene_space,
-            num_generations=num_generations,
-            num_parents_mating=num_parents_mating,
-            sol_per_pop=sol_per_pop,
-            num_genes=num_genes,
+            num_generations= self.parameters['num_generations'] if self.parameters['num_generations'] else num_generations,
+            num_parents_mating = self.parameters['num_parents_mating'] if self.parameters['num_parents_mating'] else num_parents_mating,
+            sol_per_pop = self.parameters['sol_per_pop'] if self.parameters['sol_per_pop'] else sol_per_pop,
+            num_genes = self.parameters['num_genes'] if self.parameters['num_genes'] else num_genes,
             init_range_low=init_low_range,
             init_range_high=init_high_range,
             random_mutation_min_val=random_mutation_min_val,
@@ -87,8 +93,14 @@ class Optmizer():
         )
 
         # Running the GA to optimize the parameters of the function.
+        start_time = timer()
         ga_instance.run()
+        end_time = timer()
+        self.results['time_elapsed'] = end_time - start_time
+        
+        fig, ax = plt.subplots(figsize=(15, 8))
         ga_instance.plot_fitness().savefig('graphs/optimization_fitness.png')
+        self.results['fitness_graph'] = fig
 
 
         # Returning the details of the best solution.
@@ -101,8 +113,15 @@ class Optmizer():
         if ga_instance.best_solution_generation != -1:
             print("Best fitness value reached after {best_solution_generation} generations.".format(best_solution_generation=ga_instance.best_solution_generation))
 
-        self.solution = solution
+        self.results['solution'] = solution
+        self.results['solution_fitness'] = solution_fitness
+        self.results['solution_idx'] = solution_idx
+        self.results['ga_instance'] = ga_instance
         self.__has_runned = True
+
+        solution = {
+            assets_data.columns.values[i]: percentage for i, percentage in enumerate(solution)
+        }
         return solution
 
         def save_instance(self):
@@ -113,4 +132,3 @@ class Optmizer():
             # Loading the saved GA instance.
             loaded_ga_instance = pygad.load(filename=self.filename)
             loaded_ga_instance.plot_fitness()
-
