45ebe47ea4466876b335fb491699b4d9106eb82d
WagnoLeaoSergio <wagnoleao@gmail.com>
2022-02-12 21:38:17 -0300

    module dashboard added

--- portfolio_auto_analysis/__init__.py
+++ portfolio_auto_analysis/__init__.py
@@ -1,11 +1,13 @@
 from .base import BaseClass, base_function
 from .portfolio import proto, Portfolio_Analyzer
 from .optimization import Optmizer
+from .dashboard import Dashboard
 
 __all__ = [
         "BaseClass",
         "base_function",
         "proto",
         "Portfolio_Analyzer",
-        "Optmizer"
+        "Optmizer",
+        "Dashboard"
     ]
